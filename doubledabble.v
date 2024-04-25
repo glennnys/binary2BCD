42b49a6a8d4c1193ca86b1643c2880d49469f746
glennnys <79157749+glennnys@users.noreply.github.com>
2024-04-25 23:22:44 +0200

    Fixed a problem where a statement didn't cover every case
    i > j didn't work for larger values

--- doubledabble.v
+++ doubledabble.v
@@ -2,7 +2,7 @@
 
 
 module doubledabble #(
-    parameter BITS = 16,
+    parameter BITS = 31, //31 is the maximum for some reason, I don't know why
     localparam integer NIBBLES = $ceil($log10(2**(BITS-1))),
     localparam integer OUTBITS = NIBBLES*4
     
@@ -12,19 +12,20 @@ module doubledabble #(
     );
     
     reg [OUTBITS-1:0] data [BITS:0];
-    reg [OUTBITS-1:0] intermediate [BITS:0];
+    reg [OUTBITS-1:0] intermediate [BITS-1:0];
     integer i, j;
+    
     always @(bin) begin
         data[0] = 0;
         for (i = 0; i < BITS; i = i + 1) begin
             for (j = 0; j < OUTBITS; j = j + 4) begin
-                if (i > j) begin
-                    if (data[i][j+:4] >= 5) intermediate[i][j+:4] = data[i][j+:4] + 3;		//If any BCD digit is >= 5, add 	
+                if ($ceil(4*$log10(2**(i))) > j + 1) begin
+                    if (data[i][j+:4] >= 5) intermediate[i][j+:4] = data[i][j+:4] + 3;	
                     else begin intermediate[i][j+:4] = data[i][j+:4]; end
                 end
                 else begin intermediate[i][j+:4] = data[i][j+:4]; end
             end
-            data[i+1] = {intermediate[i][BITS:0],bin[(BITS-1)-i]};
+            data[i+1] = {intermediate[i][OUTBITS-2:0],bin[(BITS-1)-i]};
         end
     end 
     
